04d325358379ea8aeb1e8444abc35c71732b7c65
Harsha <harsha.13cs403@gmail.com>
2019-06-28 12:26:24 +0530

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+	
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 
-# Create your views here.
+#Function post_list is created where post_list takes request and will return the value
+#that gets from render function that will render our template blog/post_list.html
